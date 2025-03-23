c0952abc0a41d8ee36bbdde09138d1fdccda17ad
MundoEriclene <mundoericlene@gmail.com>
2025-03-23 14:57:34 +0000

    Removendo pastas

--- app/core/notificacoes.py
+++ app/core/notificacoes.py
@@ -1,5 +1,28 @@
 import os
 import requests
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_DESTINO = os.getenv("EMAIL_DESTINO")
+
+def enviar_email(assunto, mensagem):
+    try:
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL_SENDER
+        msg["To"] = EMAIL_DESTINO
+        msg["Subject"] = assunto
+
+        msg.attach(MIMEText(mensagem, "plain"))
+        contexto = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as server:
+            server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+            server.sendmail(EMAIL_SENDER, EMAIL_DESTINO, msg.as_string())
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
